b6084f39db8de4d066ccb4e0d1147311c7714947
junielbrizola <junnybrizola39@gmail.com>
2021-04-05 20:19:57 -0300

    feat: :sparkles: backend

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -39,8 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
-            \Fruitcake\Cors\HandleCors::class,
+            'bindings'
         ],
     ];
 
